002f4e559d11398e541d576b9fad08e7c32d544f
Aviral Singh <2k24.csds1b.2411599@gmail.com>
2026-07-22 14:44:36 +0530

    Complete Milestone 2 Day 7 Product Recommendation API

---
+++ AIML/recommendation_api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load recommendation data
+recommendations = pd.read_csv("../data/product_recommendations.csv")
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Product Recommendation API is Running"
+    })
+
+
+@app.route("/recommend/<product_id>")
+def recommend(product_id):
+
+    product_id = product_id.upper()
+
+    result = recommendations[
+        recommendations["ProductID"] != product_id
+    ].head(3)
+
+    return jsonify({
+        "ProductID": product_id,
+        "Recommendations": result["ProductID"].tolist()
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
