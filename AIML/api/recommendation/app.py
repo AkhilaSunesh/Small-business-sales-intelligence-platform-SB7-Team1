66a7481048755488972cb112dc3f50b112b94674
Aviral Singh <2k24.csds1b.2411599@gmail.com>
2026-07-29 16:15:08 +0530

    Update AIML API ports for backend integration

--- AIML/api/recommendation/app.py
+++ AIML/api/recommendation/app.py
@@ -30,4 +30,4 @@ def recommend(product_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5012)
--- AIML/api/segmentation/app.py
+++ AIML/api/segmentation/app.py
@@ -35,4 +35,4 @@ def get_customer_group(customer_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5010)
